baaed9d29874cfc55489e7a1aca5f9662f00ea8f
enricclemente <enricclemente59@gmail.com>
2024-11-10 11:57:43 +0100

    Update page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -12,9 +12,9 @@ export default function Projects() {
         <Title className="text-5xl sm:text-6xl md:text-7xl leading-[1.2] text-center">
           My work.
         </Title>
-        <p className="text-[18px] sm:text-[20px] md:text-[23px] leading-[22px] sm:leading-[30px] md:leading-[35px] text-center md:text-balance max-w-[32ch] md:max-w-[65ch]">
-         I&apos;m a UX Designer, passionate about crafting experiences that are
-        engaging, accessible, and user-centric.
+        <p className="text-[18px] sm:text-[20px] md:text-[23px] leading-[22px] sm:leading-[30px] md:leading-[35px] text-center md:text-balance max-w-[42ch] md:max-w-[72ch]">
+          I&apos;m a UX Designer, passionate about crafting experiences that are
+          engaging, accessible, and user-centric.
         </p>
       </div>
       <div className="py-12 px-5 md:py-16 md:px-20 w-full flex justify-center">
